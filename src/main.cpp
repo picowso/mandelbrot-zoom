3a298d1787376a25887979bc9e226353c5be0b6b
zaki98 <zakariaabissourour@gmail.com>
2026-07-05 16:11:32 +0100

    black & white + improved color function

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,17 @@ using std::swap;
 constexpr int WINDOW_WIDTH = 640;
 constexpr int WINDOW_HEIGHT = 480;
 long double xx = -2.5, xy = 1.0, yx = -1.5, yy = 1.5; // i lwk dont know wtf these are
-int good(long double x, long double y) {
+sf::Color getColor(long double x, long double y) {
     long double zx = 0., zy = 0.;
     for(int j = 0 ; j < iters_n ; j++) {
         long double zzx = zx;
         zx = zx*zx - zy*zy + x;
         zy = 2*zzx*zy + y;
-        if(zx*zx + zy*zy > 4.) return j;
+        if(zx*zx + zy*zy > 4) return sf::Color::White;
     }
-
-    return -1;
+    return sf::Color::Black;
 }
-void zoomIn(float coefficient) {
+void zoomIn(long double coefficient) {
     xx = zom_ptx + (xx - zom_ptx) * coefficient;
     xy = zom_ptx + (xy - zom_ptx) * coefficient;
     yx = zom_pty + (yx - zom_pty) * coefficient;
@@ -29,19 +28,10 @@ void getRawImage(int arr[WINDOW_WIDTH][WINDOW_HEIGHT][3]) {
             long double x = xx + ((k+1)*(xy-xx))/WINDOW_WIDTH;
             long double y = yx + ((j+1)*(yy-yx))/WINDOW_HEIGHT;
             // cout << yay << endl;
-            int v = good(x, y);
-            int r, g, b;
-            if(v == -1) {
-                // SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-            }
-
-            else {
-                v = (255*v)/iters_n;
-                // SDL_SetRenderDrawColor(renderer, v, v, v, 255);
-                for(int q = 0 ; q < 3 ; q++) arr[k][j][q] = v;
-            }
-
-            // SDL_RenderPoint(renderer, (long double)k, (long double)j);
+            sf::Color v = getColor(x, y);
+            arr[k][j][0] = v.r;
+            arr[k][j][1] = v.g;
+            arr[k][j][2] = v.b;
         }
     }
 }
@@ -80,6 +70,7 @@ int main()
     sf::RenderWindow window( sf::VideoMode( { WINDOW_WIDTH, WINDOW_HEIGHT } ), "Mandelbrot zoom",
         sf::Style::Close, sf::State::Windowed, settings);
     window.setFramerateLimit(60);
+    int frame = 0;
     while ( window.isOpen() )
     {
         while ( const std::optional event = window.pollEvent() )
@@ -94,7 +85,17 @@ int main()
         int afterGaussian[WINDOW_WIDTH][WINDOW_HEIGHT][3] = {};
         gaussianBlur(afterGaussian,beforeGaussian);
         renderImage(afterGaussian,window);
-        zoomIn(0.99);
+
+        sf::Texture texture(window.getSize());
+        texture.update(window);
+        if (texture.copyToImage().saveToFile(std::format("Frame {}.png", frame)))
+        {
+        }
+        else {
+            throw std::runtime_error("Failed to save frame");
+        }
+        frame++;
+        zoomIn(0.9);
         window.display();
     }
 }
